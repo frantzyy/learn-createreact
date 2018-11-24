942fdc2dede9d9faef260f1256046fc175a93e43
Chris Frantz <frantzyy@yahoo.com>
2018-11-24 16:49:06 -0500

    moved state to app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counters from './components/counters';
+import Navbar from './components/navbar';
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleDelete = counterId => {
+    console.log('handleDelete called! ', counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    //get the existing counters, reset values to 0 and return it
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    console.log('handle increment', counter);
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div className="App">
@@ -22,7 +56,15 @@ class App extends Component {
           </a>
         </header>
         <h1>Hello World Joe!</h1>
-        <Counters />
+        <Navbar />
+        <main className="container">
+          <Counters
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            counters={this.state.counters}
+          />
+        </main>
       </div>
     );
   }
